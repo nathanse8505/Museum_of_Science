0b1805217d529d5a91156335c6afaf27d51125ab
nathanse8505 <nathanse8505@gmail.com>
2024-09-29 20:06:14 +0300

    version 3
    4 7seg

--- acceleration_circuit/speedometer/test_7segment/CONSTANTS_test.h
+++ acceleration_circuit/speedometer/test_7segment/CONSTANTS_test.h
@@ -5,6 +5,7 @@
 #define BCD_C (6) // Output to BCD_C (4511)
 #define BCD_D (7) // Output to BCD_D (4511)
 
-#define LE_LEFT (10) // Output Left (100th) digit LE (negative pulse) used as 10th
-#define LE_MIDDLE (8)
-#define LE_RIGHT (9) // Output right (10th) digit LE (negative pulse) used as units 
+#define LE_CENTURIES (9) // Output Left (100th) digit LE (negative pulse) used as 10th
+#define LE_DOZENS (11)
+#define LE_UNITS (10) // Output right (10th) digit LE (negative pulse) used as units 
+#define LE_THOUSANDS (8)
--- acceleration_circuit/speedometer/test_7segment/test_7segment.ino
+++ acceleration_circuit/speedometer/test_7segment/test_7segment.ino
@@ -1,6 +1,7 @@
 #include "CONSTANTS_test.h"
 
 int Number_To_Display = 0;
+int Digit_4_To_Display = 0;
 int Digit_3_To_Display = 0;// Left (100th) digit to disply - used as 10th
 int Digit_2_To_Display = 0;// Mid right (10th) digit to disply  - used as units 
 int Digit_1_To_Display = 0;// rigth (external unit) digit to disply - not used for winf tunnel
@@ -13,9 +14,10 @@ void Init_Output(int IO_Pin){
   digitalWrite(IO_Pin, LOW);/* make sure low  output */
 }
 void Digits_from_Number(int in_number){
-    Digit_3_To_Display = in_number/100;// left digit to disply 
-    int Temp_10 = in_number - 100 * Digit_3_To_Display;
-    Digit_2_To_Display = Temp_10/10;// mid digit to disply 
+    Digit_4_To_Display =  in_number / 1000;
+    Digit_3_To_Display = (in_number - 1000 *Digit_4_To_Display)/100;// left digit to disply 
+    //int Temp_10 = in_number - 100 * Digit_3_To_Display;
+    Digit_2_To_Display = (in_number - 100 * Digit_3_To_Display)/10;// mid digit to disply 
     Digit_1_To_Display = in_number % 10 ;// right digit to disply 
   //Serial.println(Digit_3_To_Display);
   //Serial.println(Digit_2_To_Display);
@@ -31,28 +33,31 @@ void Display_Digit(int digit_to_show) {
     if ((digit_to_show & 4) == 4) {digitalWrite(BCD_C, HIGH);}
     digitalWrite(BCD_D, LOW);// asume bit zero 
     if ((digit_to_show & 8) == 8) {digitalWrite(BCD_D, HIGH);}  
+    
+    
 }
 
 void enable(int digit_enable){
  switch(digit_enable) {
       case 1: //range 0-9  '00000110'
-       digitalWrite(LE_RIGHT, LOW);
-       digitalWrite(LE_RIGHT, HIGH); 
+       digitalWrite(LE_UNITS, LOW);
+       digitalWrite(LE_UNITS, HIGH); 
        break;
-
       case 2: //range 9-99 '000000101'
-       digitalWrite(LE_MIDDLE, LOW);
-       digitalWrite(LE_MIDDLE, HIGH); 
+       digitalWrite(LE_DOZENS, LOW);
+       digitalWrite(LE_DOZENS, HIGH); 
+       break;
+      case 3: //range 100-999 '00000011'
+       digitalWrite(LE_CENTURIES, LOW);
+       digitalWrite(LE_CENTURIES, HIGH); 
+       case 4:
+       digitalWrite(LE_THOUSANDS, LOW);
+       digitalWrite(LE_THOUSANDS, HIGH); 
        break;
       
-      case 3: //range 100-999 '00000011'
-       digitalWrite(LE_LEFT, LOW);
-       digitalWrite(LE_LEFT, HIGH); 
-       break;
       
     }
 }
-
 
 
 void setup() {
@@ -62,31 +67,43 @@ void setup() {
   Init_Output(BCD_B);
   Init_Output(BCD_C);
   Init_Output(BCD_D);
-  Init_Output(LE_LEFT);
-  digitalWrite(LE_LEFT, HIGH);// lock digit
-  Init_Output(LE_MIDDLE);
-  digitalWrite(LE_MIDDLE, HIGH);// lock digit
-  Init_Output(LE_RIGHT);
-  digitalWrite(LE_RIGHT, HIGH);// lock digit
+  
+ 
+  Init_Output(LE_UNITS);
+  digitalWrite(LE_UNITS, HIGH);// lock digit
+
+  Init_Output(LE_DOZENS);
+  digitalWrite(LE_DOZENS, HIGH);// lock digit
+
+  Init_Output(LE_CENTURIES);
+  digitalWrite(LE_CENTURIES, HIGH);// lock digit
+
+  Init_Output(LE_THOUSANDS);
+  digitalWrite(LE_THOUSANDS, HIGH);// lock digit
+
 }
 
 int i = 0;
 void loop() {
     Digits_from_Number(i);
-    Display_Digit(Digit_1_To_Display);
+    /*Display_Digit(Digit_1_To_Display);
     enable(1);
     Display_Digit(Digit_2_To_Display);
-    enable(2);
-    Display_Digit(Digit_3_To_Display);
+    enable(2);*/
+
+    //Display_Digit(Digit_3_To_Display); 
+    Display_Digit(8); 
     enable(3);
-    delay(500);
-    i++;
-    if(i == 1000)
-    {
-      i=0;
-    }
-    
-
-
+    Display_Digit(8);
+    //Display_Digit(Digit_4_To_Display);
+    enable(4);
+    delay(1000);
+ /* Serial.println(i);
+  Serial.print("centurie ");
+  Serial.println(Digit_3_To_Display);
+  Serial.print("Tousand ");
+  Serial.println(Digit_4_To_Display);*/
+    i+=100;
+    if(i == 10000){i=0;}
 }
 
