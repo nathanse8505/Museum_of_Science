93146a904903fd1b26d1d57f7c0f756896ca1516
nathanse8505 <nathanse8505@gmail.com>
2024-10-01 21:58:11 +0300

    version 3
    constant for 4 7seg

--- acceleration_circuit/speedometer/speedometer_7segment/speedometer_7segment_4disp.ino/CONSTANTS.h
+++ acceleration_circuit/speedometer/speedometer_7segment/speedometer_7segment_4disp.ino/CONSTANTS.h
@@ -5,10 +5,14 @@
 #define BCD_C (6) // Output to BCD_C (4511)
 #define BCD_D (7) // Output to BCD_D (4511)
 
-#define LE_CENTURIES (11) // Output Left (100th) digit LE (negative pulse) used as 10th
-#define LE_DOZENS (9)
-#define LE_UNITS (8) // Output right (10th) digit LE (negative pulse) used as units 
-#define LE_THOUSANDS (10)
+#define LE_CENTURIES (9) // Output Left (100th) digit LE (negative pulse) used as 10th
+#define LE_DOZENS (11)
+#define LE_UNITS (10) // Output right (10th) digit LE (negative pulse) used as units 
+#define LE_THOUSANDS (8)
 
 #define IN_FET 3
 #define OUT_FET 2
+
+
+
+
