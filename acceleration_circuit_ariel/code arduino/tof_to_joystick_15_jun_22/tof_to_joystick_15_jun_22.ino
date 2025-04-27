34c613f9b0a7e0883f530e1e456cbbf789910ac7
Nathan Sebag <nathans@mada.org.il>
2025-04-27 20:29:50 +0300

    new

--- acceleration_circuit_ariel/code arduino/tof_to_joystick_15_jun_22/tof_to_joystick_15_jun_22.ino
+++ acceleration_circuit_ariel/code arduino/tof_to_joystick_15_jun_22/tof_to_joystick_15_jun_22.ino
@@ -1,10 +1,10 @@
 /*
- * tof_to_joystick - reads distance measurements from VL53L1X TimeOfLight sensor,
+ * tof_to_joystick - reads distance measurements from VL53L0X TimeOfLight sensor,
  * writes the results to the USB joystick (Pro Micro ATmega32U4).
  * 
  * Pinout:
  *  ┌─────────┬─────────┐
- *  │ Arduino │ VL53L1X │
+ *  │ Arduino │ VL53L0X │
  *  ├─────────┼─────────┤
  *  │ VCC     │ VCC     │
  *  │ GND     │ GND     │
@@ -12,55 +12,43 @@
  *  │ D3      │ SCL     │
  *  └─────────┴─────────┘
  * Arad Eizen 25/12/21.
- * 2/4/2025 - Modified to VL53L1X sensor (originally VL53L1X)
  */
-
-
- 
 #include <Arduino.h>
 #include <HID-Project.h>
 
-#define LOOP_MS					(20)
+#define LOOP_MS					(50)
 #define TOF_MAX_VALUE			(3000)
 #define JOYSTICK_MIN_VALUE		(12000)
 #define JOYSTICK_MAX_VALUE		(20000)
 
 #include <Wire.h>
-#include <VL53L1X.h>			// https://github.com/pololu/vl53l1x-arduino
+#include <VL53L0X.h>			// https://github.com/pololu/vl53l0x-arduino
 
-VL53L1X tof;
-
-int16_t dist = 0;
+VL53L0X tof;
 
 void setup()
 {
-	
 	// initialize USB joystick HID
 	Gamepad.begin();
+
 	// initialize TOF sensor to long range readings
 	Wire.begin();
 	tof.setTimeout(500);
-	while (!tof.init()){
-    delay(1000);
-    //Serial.println("error");
-   }
+	while (!tof.init())
+		delay(1000);
 
-  tof.setDistanceMode(VL53L1X::Long);
-  tof.setMeasurementTimingBudget(50000);
-	tof.startContinuous(50);
+	tof.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
+	tof.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
+	tof.startContinuous();
 }
 
 void loop()
 {
-  dist = tof.read();
+	uint16_t dist = tof.readRangeContinuousMillimeters();
 	if (tof.timeoutOccurred())
 		dist = 0;
 
-	 // Mapper la distance en valeur de joystick
-  int16_t joystickValue = map(dist, 0, TOF_MAX_VALUE, JOYSTICK_MIN_VALUE, JOYSTICK_MAX_VALUE);
-
-  // Envoi au joystick
-  Gamepad.xAxis(joystickValue);
-  Gamepad.write();
+	Gamepad.xAxis(map(dist, 0, TOF_MAX_VALUE, JOYSTICK_MIN_VALUE, JOYSTICK_MAX_VALUE));
+	Gamepad.write();
 	delay(LOOP_MS);
 }
