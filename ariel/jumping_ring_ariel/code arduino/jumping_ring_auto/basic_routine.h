a8954dea741a5df4ef9516fdbbe25ffcd95f384c
nath <nathanse8505@gmail.com>
2025-09-01 21:46:15 +0300

    new

---
+++ ariel/jumping_ring_ariel/code arduino/jumping_ring_auto/basic_routine.h
@@ -0,0 +1,75 @@
+#ifndef basic_routine
+#define basic_routine
+
+#include <Arduino.h>
+#include <avr/wdt.h>
+
+/*
+*=================Arduino Nano pinout==================
+ *                      _______
+ *                 TXD-|       |-Vin 
+ *                 RXD-|       |-Gnd  
+ *                 RST-|       |-RST
+ *                 GND-|       |-+5V  
+ *  ignition button D2-|       |-A7  
+ *   LED_ACTIVATION D3-|       |-A6  
+ *                  D4-|       |-A5 
+ *            RELAY D5-|       |-A4 
+ *                  D6-|       |-A3
+ *                  D7-|       |-A2   
+ *                  D8-|       |-A1   
+ *                  D9-|       |-A0   
+ *                 D10-|       |-Ref
+ *                 D11-|       |-3.3V   
+ *                 D12-|       |-D13
+ *                      --USB--        
+ */
+#define BAUDRATE (115200)
+////////////  I/O   ////////////
+#define BUTTON_IO       2
+#define LED_ACTIVATION  3
+#define RELAY_IO        7  // relay active contactor 
+// global vars
+int32_t time_start = 0;
+int32_t time_new_session = 0;
+bool flag_new_session = true;
+
+const uint32_t ACTIVATION_TIME = 10000;
+const uint32_t TIME_RELAY_ON =2000;
+
+/////////button ignition//////////
+bool check_ignit = LOW;
+bool buttonPressed;
+bool flag_first_press = false;
+const int16_t BOUNCE_TIME = 100;//ms 
+//////////////////////////////////
+
+
+
+bool PRESS_BUTTON_IGNITION() {
+   wdt_reset();//reset the watchdog
+  // Check if the button is pressed
+  if (digitalRead(BUTTON_IO) == LOW && check_ignit == LOW) {
+     //Serial.println("press :");
+     check_ignit = HIGH;         // Mark that the button is being pressed
+    delay(BOUNCE_TIME); // Apply debounce delay
+  }
+
+  // Check if the button is released
+  if (digitalRead(BUTTON_IO) == HIGH && check_ignit == HIGH) {
+    //Serial.println("unpress");
+    check_ignit = LOW;  // Reset the state for the next button press
+    return HIGH;  // Indicate that the button was successfully pressed and released
+  }
+  return LOW; // Return false if the button is not in the desired state
+}
+
+void reset_session() {
+  flag_first_press = false;
+  flag_new_session = true;
+  time_start = millis();
+}
+
+
+
+#endif
---
+++ ariel/jumping_ring_ariel/code arduino/jumping_ring_auto/jumping_ring_auto.ino
@@ -0,0 +1,55 @@
+#include "basic_routine.h"
+
+
+void setup() {
+  // Configure pin modes
+  pinMode(BUTTON_IO, INPUT_PULLUP);  // Ignition button (active LOW)
+  pinMode(LED_ACTIVATION, OUTPUT);   // Activation indicator LED
+  pinMode(RELAY_IO, OUTPUT);         // relay control Contactor Coil
+
+  Serial.begin(BAUDRATE);            // Start serial communication
+
+  Serial.println("init");
+  wdt_enable(WDTO_4S);  
+}
+
+void loop() {
+
+  wdt_reset();
+
+  // Check if ignition button is pressed
+  //buttonPressed = true;
+  buttonPressed = PRESS_BUTTON_IGNITION();
+
+
+  // After some time, move on to ignition phase
+  if ((millis() - time_start) > ACTIVATION_TIME) {
+    
+    // If it's the first entry into this phase
+    if(flag_new_session){
+
+      digitalWrite(LED_ACTIVATION, HIGH);  // Turn on activation LED  
+      flag_new_session = false;             // new Session ready
+      Serial.println("we can push the button");
+      Serial.println("led activated");
+      Serial.println();
+    }
+
+    // On first button press (ignition)
+    if (buttonPressed && flag_first_press == false) {
+      digitalWrite(LED_ACTIVATION, LOW);   // Turn off activation LED
+      digitalWrite(RELAY_IO, HIGH);        // the ring jump
+      flag_first_press = true;            // Mark ignition as pressed
+      time_new_session = millis();
+      Serial.println("button has been pressed");
+      Serial.println("the ring jumping");
+    }
+    
+    if (millis() - time_new_session > TIME_RELAY_ON && flag_first_press){
+      digitalWrite(RELAY_IO, LOW);
+      reset_session();
+    }
+    delay(1);
+  }
+
+}
